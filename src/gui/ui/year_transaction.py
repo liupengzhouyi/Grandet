f3b7eee9f75657d6026254bd8d05c4b484a56c4f
liupeng <liupeng.0@outlook.com>
2023-07-12 23:23:04 +0800

    Add month page.

--- src/gui/ui/year_transaction.py
+++ src/gui/ui/year_transaction.py
@@ -14,10 +14,11 @@ from functions.log4py import print_log
 from gui.ui.globel_varable import set_value
 from gui.ui.globel_varable import get_value
 
-from gui.ui.year_transaction import create_grandet_bills_window_by_year
-
 from functions.read_table import ReadTransactionTable
+from functions.log4py import print_log
+
 from modules.transaction import YearsTransaction
+from modules.transaction import MonthsTransaction
 from modules.transactions_tools import TransactionsTools
 
 
@@ -47,7 +48,7 @@ def fill_bill_files(bill_frame: tk.Frame, file_names: list) -> tk.Frame:
     return bill_frame
 
 
-def fill_bill_information(yearly_summary: tk.Frame, root: tk.Tk, head_words: list, years: list, image_path: str) -> tk.Frame:
+def fill_bill_information(yearly_summary: tk.Frame, root: tk.Tk, head_words: list, months: list) -> tk.Frame:
     
     # 右侧每年花销简介
     # yearly_summary = tk.Frame(root)
@@ -81,49 +82,30 @@ def fill_bill_information(yearly_summary: tk.Frame, root: tk.Tk, head_words: lis
     separator = tk.Frame(yearly_summary, height=2, bd=1, relief="sunken")
     separator.pack(fill="x", padx=5, pady=5)
 
-    for year, summary, expenditure_count, income_count, transfer_count, all_transfer_count in years:
-        year_row = tk.Frame(yearly_summary)
-        year_row.pack(side="top", fill="x")
+    for month, summary, expenditure_count, income_count, transfer_count, all_transfer_count in months:
+        month_row = tk.Frame(yearly_summary)
+        month_row.pack(side="top", fill="x")
 
-        year_label = tk.Label(year_row, text=year, width=10)
-        year_label.pack(side="left")
+        month_label = tk.Label(month_row, text=month, width=10)
+        month_label.pack(side="left")
 
-        summary_label = tk.Label(year_row, text=str(f"{summary:.2f}"), width=10)
+        summary_label = tk.Label(month_row, text=str(f"{summary:.2f}"), width=10)
         summary_label.pack(side="left")
 
-        expenditure_label = tk.Label(year_row, text=str(expenditure_count), width=15)
+        expenditure_label = tk.Label(month_row, text=str(expenditure_count), width=15)
         expenditure_label.pack(side="left")
 
-        income_label = tk.Label(year_row, text=str(income_count), width=15)
+        income_label = tk.Label(month_row, text=str(income_count), width=15)
         income_label.pack(side="left")
 
-        transfer_label = tk.Label(year_row, text=str(transfer_count), width=20)
+        transfer_label = tk.Label(month_row, text=str(transfer_count), width=20)
         transfer_label.pack(side="left")
         
-        transfer_label = tk.Label(year_row, text=str(all_transfer_count), width=20)
+        transfer_label = tk.Label(month_row, text=str(all_transfer_count), width=20)
         transfer_label.pack(side="left")
 
-        # details_button = tk.Button(year_row, text="详情", command=lambda y=year, r=root: show_details(r, y), width=10)
-        details_button = tk.Button(year_row, text="详情", command=lambda y=year: create_grandet_bills_window_by_year(y), width=10)
-        
+        details_button = tk.Button(month_row, text="详情", command=lambda y=month, r=root: show_details(r, y), width=10)
         details_button.pack(side="right")
-    
-    separator_for_image = tk.Frame(yearly_summary, height=2, bd=1, relief="sunken")
-    separator_for_image.pack(fill="x", padx=5, pady=5)
-    
-    canvas = tk.Canvas(yearly_summary, width=200, height=200)
-    
-    print(f"Read image: {image_path}.")
-    # 打开图像并转换为PhotoImage对象
-    img = Image.open(image_path)
-    img_resized = img.resize((200,200), Image.ANTIALIAS) # 调整图片大小
-
-    photo = ImageTk.PhotoImage(img_resized, master=canvas)
-
-    # 在画布上插入图像
-    canvas.create_image(100, 100, anchor=tk.CENTER, image=photo)
-    canvas.pack()
-    # root.mainloop()
 
     return yearly_summary
     
@@ -136,52 +118,61 @@ def show_details(root: tk.Tk, year: str):
     details_label.pack()
 
 
-def get_data() -> tuple:
+def get_data_by_year(year: int) -> tuple:
         
     files = []
     years = []
-    print_log(f"get every_years_transactions")
+    print_log(f"get {str(year)} year transactions")
     every_years_transactions = get_value("every_years_transactions")
     if every_years_transactions is None:
         print_log(f"get every_years_transactions error.")
         # 账单文件
         files = ["账单1", "账单2", "账单3"]
-        years = [("2020", 1000, 10, 5, 3, 18),
-                 ("2021", 2000, 15, 8, 4, 27),
-                 ("2022", 3000, 20, 10, 5, 35)]
+        years = [("1", 1000, 10, 5, 3, 18),
+                 ("2", 2000, 15, 8, 4, 27),
+                 ("3", 3000, 20, 10, 5, 35)]
     else:
-        print_log(f"get every_years_transactions success.")
-        all_files = get_value("bills_files")
-        files = []
-        for item in all_files:
-            files.append(os.path.basename(item))
-        years = []
-        for key_word in every_years_transactions.keys():
+        year_transaction = every_years_transactions.get(str(year))
+        
             
-            year_transaction = every_years_transactions.get(key_word)
-            if isinstance(year_transaction, YearsTransaction):
-                year = year_transaction.year
-                all_number = TransactionsTools.get_transactions_size(year_transaction)
-                summary = round(sum(TransactionsTools.get_amounts(year_transaction)), 3)
-                expenditure_count = TransactionsTools.get_summary_count(year_transaction)
-                income_count = TransactionsTools.get_income_count(year_transaction)
-                transfer_count = TransactionsTools.get_transfer_count(year_transaction)
+        if year_transaction is not None:
+            
+            print_log(f"get every_years_transactions success.")
+            all_files = get_value("bills_files")
+            files = []
+            for item in all_files:
+                files.append(os.path.basename(item))
                 
-                years.append((year, summary, expenditure_count, income_count, transfer_count, all_number))
-            else:
-                continue
+            years = []
+            month_transactions = []
+            if isinstance(year_transaction, YearsTransaction):
+                month_transactions = year_transaction.to_MonthsTransaction()
+            
+            for month_transaction_item in month_transactions.keys():   
+                month_transaction = month_transactions.get(month_transaction_item)
+                if isinstance(month_transaction, MonthsTransaction):
+                    month = month_transaction.month
+                    all_number = TransactionsTools.get_transactions_size(month_transaction)
+                    summary = round(sum(TransactionsTools.get_amounts(month_transaction)), 3)
+                    expenditure_count = TransactionsTools.get_summary_count(month_transaction)
+                    income_count = TransactionsTools.get_income_count(month_transaction)
+                    transfer_count = TransactionsTools.get_transfer_count(month_transaction)
+                    
+                    years.append((month, summary, expenditure_count, income_count, transfer_count, all_number))
+                else:
+                    continue
+        else:
+            print_log(f"get every months transactions error.")
+    return (files, years)
 
-    image_path = "/Users/pengliu/Code/Grandet/src/gui/ui/images.png" 
-    return (files, years, image_path)
 
-
-def create_grandet_bills_window():
+def create_grandet_bills_window_by_year(year: int):
     
     # 标题
-    title = "葛朗台的账单"
+    title = f"葛朗台{str(year)}年的账单"
     # 表头
-    head_words = ["年份", "花销总额", "支出交易笔数", "收入交易笔数", "个人转账交易笔数", "交易总笔数", "详情"]
-    files, years, image_path = get_data()
+    head_words = ["月份", "花销总额", "支出交易笔数", "收入交易笔数", "个人转账交易笔数", "交易总笔数", "详情"]
+    files, months = get_data_by_year(year)
 
     root = tk.Tk()
     root.geometry("1600x720") # 设置窗口大小
@@ -189,13 +180,13 @@ def create_grandet_bills_window():
     bill_frame = tk.Frame(root, width=150) # 设置列表框架宽度为窗口宽度的1/4
     yearly_summary = tk.Frame(root, width=600)
     
-    bill_frame = fill_bill_files(bill_frame=bill_frame, file_names=files)
+    # bill_frame = fill_bill_files(bill_frame=bill_frame, file_names=files)
     
     yearly_summary = fill_bill_information(yearly_summary=yearly_summary,
                                            root=root,
                                            head_words=head_words,
-                                           years=years,
-                                           image_path=image_path)
+                                           months=months)
+                                           
 
     years_lable = []
     expenditure_counts = []
@@ -203,9 +194,9 @@ def create_grandet_bills_window():
     transfer_counts = []
     all_transfer_counts = []
     
-    for year, summary, expenditure_count, income_count, transfer_count, all_transfer_count in years:
+    for month, summary, expenditure_count, income_count, transfer_count, all_transfer_count in months:
         
-        years_lable.append(year)
+        years_lable.append(month)
         expenditure_counts.append(expenditure_count)
         income_counts.append(income_count)
         transfer_counts.append(transfer_count)
