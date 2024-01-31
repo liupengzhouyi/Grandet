3896b3f97082613fb7d0d9d8181c0ec4e6c71520
liupeng <liupeng.0@outlook.com>
2024-01-31 16:42:31 +0800

    fix:Add unit test.

---
+++ test/test_01.py
@@ -0,0 +1,35 @@
+import unittest
+
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+# test
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        result = Calculator.add(2, 3)
+        self.assertEqual(result, 5)
+
+    def test_subtract(self):
+        result = Calculator.subtract(5, 3)
+        self.assertEqual(result, 2)
+    
+    def test_subtract1(self):
+        result = Calculator.subtract(5, 3)
+        self.assertEqual(result, 2)
+        
+    def test_subtract3(self):
+        result = Calculator.subtract(5, 3)
+        self.assertEqual(result, 2)
+    
+    def test_subtract4(self):
+        result = Calculator.subtract(5, 3)
+        self.assertEqual(result, 2)
+
+# if __name__ == '__main__':
+#     unittest.main()
